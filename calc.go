8efbf96201c887ececd4fbd07bc916178de0a210
Chunming Li <ryan.lichunming@gmail.com>
2021-03-26 18:50:02 -0400

    Update terms

--- calc.go
+++ calc.go
@@ -11,24 +11,24 @@ import (
 )
 
 type conf struct {
-	Year                         int64   `yaml:"year"`
-	Revenue                      int64   `yaml:"revenue"`
-	OperatingCost                int64   `yaml:"opCost"`
-	OperatingRevenue             int64   `yaml:"opRevenue"`
-	CostOfSales                  int64   `yaml:"costOfSales"`
-	CostOfAdministrative         int64   `yaml:"costOfAdmin"`
-	CostOfResearchAndDevelopment int64   `yaml:"costOfR&D"`
-	CostOfFinancing              int64   `yaml:"costOfFinancing"`
-	OpratingProfit               int64   `yaml:"opProfit"`
-	NetProfit                    int64   `yaml:"netProfit"`
-	IrregularProfit              int64   `yaml:"irregularProfit"`
-	TotalAssets                  int64   `yaml:"assets"`
-	TotalAssetsPreviously        int64   `yaml:"assetsFromLastTime"`
-	TotalLiabilities             int64   `yaml:"liabilities"`
-	AccountReceivable            int64   `yaml:"receivable"`
-	FixedAssets                  int64   `yaml:"fixedAssets"`
-	OperatingNetCash             int64   `yaml:"opNetCash"`
-	WeightedRoE                  float64 `yaml:"weightedRoe"`
+	Year                         int64 `yaml:"year"`
+	Revenue                      int64 `yaml:"revenue"`
+	CostOfRevenue                int64 `yaml:"costOfRevenue"`
+	OperatingRevenue             int64 `yaml:"opRevenue"`
+	CostOfSales                  int64 `yaml:"costOfSales"`
+	CostOfAdministrative         int64 `yaml:"costOfAdmin"`
+	CostOfResearchAndDevelopment int64 `yaml:"costOfR&D"`
+	CostOfFinancing              int64 `yaml:"costOfFinancing"`
+	OpratingProfit               int64 `yaml:"opProfit"`
+	NetProfit                    int64 `yaml:"netProfit"`
+	IrregularProfit              int64 `yaml:"irregularProfit"`
+	TotalAssets                  int64 `yaml:"assets"`
+	TotalAssetsPreviously        int64 `yaml:"assetsFromLastTime"`
+	TotalLiabilities             int64 `yaml:"liabilities"`
+	AccountReceivable            int64 `yaml:"receivable"`
+	FixedAssets                  int64 `yaml:"fixedAssets"`
+	OperatingNetCash             int64 `yaml:"opNetCash"`
+	TotalEquityPreviously        int64 `yaml:"equityFromLastTime"`
 }
 
 func (c *conf) getConf(filename string) {
@@ -45,7 +45,7 @@ func (c *conf) getConf(filename string) {
 
 type report struct {
 	OperatingRevenue                float64
-	OperatingCost                   float64
+	CostOfRevenue                   float64
 	SalesExpense                    float64
 	AdminExpense                    float64
 	DevExpense                      float64
@@ -56,6 +56,8 @@ type report struct {
 	Receivable                      float64
 	FixedAssets                     float64
 	WeightedRoE                     float64
+	WeightedAverageCostOfCapital    float64
+	ReturnOnInvestedCapital         float64
 	AssetsTurnoverRatio             float64
 	OperatingNetCashToNetProfit     float64
 	OperatingNetCashToRegularProfit float64
@@ -67,9 +69,12 @@ func (c *conf) toReport() *report {
 	//Leverage
 
 	RegularProfit := c.NetProfit - c.IrregularProfit
+	roe := toPercent(
+		RegularProfit*2,
+		c.TotalAssets-c.TotalLiabilities+c.TotalEquityPreviously)
 	r := report{
 		OperatingRevenue:                toPercent(c.OperatingRevenue, c.Revenue),
-		OperatingCost:                   toPercent(c.OperatingCost, c.Revenue),
+		CostOfRevenue:                   toPercent(c.CostOfRevenue, c.Revenue),
 		SalesExpense:                    toPercent(c.CostOfSales, c.Revenue),
 		AdminExpense:                    toPercent(c.CostOfAdministrative, c.Revenue),
 		DevExpense:                      toPercent(c.CostOfResearchAndDevelopment, c.Revenue),
@@ -79,7 +84,7 @@ func (c *conf) toReport() *report {
 		LiabilitiesToAssets:             toPercent(c.TotalLiabilities, c.TotalAssets),
 		Receivable:                      toPercent(c.AccountReceivable, c.TotalAssets),
 		FixedAssets:                     toPercent(c.FixedAssets, c.TotalAssets),
-		WeightedRoE:                     c.WeightedRoE,
+		WeightedRoE:                     roe,
 		AssetsTurnoverRatio:             toPercent(c.Revenue*2, (c.TotalAssets + c.TotalAssetsPreviously)),
 		OperatingNetCashToNetProfit:     toPercent(c.OperatingNetCash, c.NetProfit),
 		OperatingNetCashToRegularProfit: toPercent(c.OperatingNetCash, RegularProfit),
